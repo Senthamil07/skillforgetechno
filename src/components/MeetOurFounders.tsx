8db86b4f341d383b95f3a3e55b87b0a997179f82
Senthamil selvan <senthamil0713@gmail.com>
2026-07-30 11:10:45 +0530

    Remove Kalai from Meet Our Founders component
    Removed Kalai from the founders list and its associated details.

--- src/components/MeetOurFounders.tsx
+++ src/components/MeetOurFounders.tsx
@@ -6,7 +6,7 @@ import { motion } from "motion/react";
 import ganeshImg from "../assets/images/regenerated_image_1782709084982.jpg";
 import hariImg from "../assets/images/regenerated_image_1782709086190.jpg";
 import tamilImg from "../assets/images/regenerated_image_1782709087886.jpg";
-import kalaiImg from "../assets/images/regenerated
+
 import veathImg from "../assets/images/regenerated_image_1782709090512.jpg";
 
 interface Founder {
@@ -44,14 +44,7 @@ export const MeetOurFounders: React.FC = () => {
       linkedin: "https://www.linkedin.com/in/senthamil-selvan-s-31531230b",
       imageUrl: tamilImg,
     },
-   {
-      id: "kalai",
-      name: "Kalai",
-      designation: "Co-Founder & Chief Partner Business Officer (CPBO)",
-      description: "Every placement is the beginning of a new success story.",
-      linkedin: "https://www.linkedin.com/in/kalai-skillforge/",
-      imageUrl: kalaiImg,
-    },
+  
     {
       id: "veath",
       name: "Veath Prakash",
